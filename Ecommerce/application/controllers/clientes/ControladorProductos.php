96936bd8f55fa31fe9ddf3fab5740ff9ed379baa
Edwin Marquez <edwin.marquez261@hotmail.com>
2020-05-15 10:04:16 -0500

    vista descripcion de productos y ciclo de articulos

--- Ecommerce/application/controllers/clientes/ControladorProductos.php
+++ Ecommerce/application/controllers/clientes/ControladorProductos.php
@@ -8,4 +8,10 @@ class ControladorProductos extends CI_Controller {
 		$this->load->view('clientes/productos/vista_Productos');
 		$this->load->view('clientes/componentes/footer');
 	}
+
+	public function descripcion()
+	{
+		$this->load->view('clientes/productos/vista_descripcion');
+		$this->load->view('clientes/componentes/footer');
+	}
 }
