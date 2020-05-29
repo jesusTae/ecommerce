94e5cdf196793126c26ecd026a63aa62aaff654d
crhistian27 <crhisjose27@gmail.com>
2020-05-28 19:56:09 -0500

    actaulizacion 28-05-2020

--- Ecommerce/application/views/clientes/welcome.php
+++ Ecommerce/application/views/clientes/welcome.php
@@ -1,4 +1,7 @@
 <br><br><br>
+<link rel="stylesheet" href="<?php echo base_url('asset/clientes/css/card.css')?>">
+  <!-- Owl-carousel CDN -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha256-UhQQ4fxEeABh4JrcmAJ1+16id/1dnlOEVCFOxDef9Lw=" crossorigin="anonymous" />
 <div class="container">
 	<button class="btn btn-success flotante totalCarritoBtn">
 		<span class="badge badge-light totalCarrito"></span> 
@@ -29,7 +32,67 @@
 			<span class="sr-only">Next</span>
 		</a>
 	</div>
+	<!-- PRODUCTOS NUVEOS -->
+        
+		<section id="new-phones">
+            <div class="container py-5">
+                <h4 class="font-rubik font-size-20">New Phones</h4>
+                <hr>
+                    <!-- owl carousel -->
+                <div class="owl-carousel owl-theme">
+					<?php 	
+						$rest = $this->db->query("SELECT n.*, a.imageurl, a.nomart FROM tbl_nuevoarticulo n INNER JOIN tbl_articulos a ON a.id  = n.n_id "); 
+						foreach ($rest->result()as $row): 
+					?>
+					<!--
+                    <div class=" py-2 bg-light">
+                        <div class="product font-rale">
+                            <a href="#"><img src="<?php //base_url('').$row->imageurl?>" alt="product1" class="img-fluid"></a>
+                            <div class="text-center">
+                                <h6>Samsung Galaxy 10</h6>
+                                <div class="rating text-warning font-size-12">
+                                  	<span><i class="fas fa-star"></i></span>
+                                  	<span><i class="fas fa-star"></i></span>
+                                  	<span><i class="fas fa-star"></i></span>
+                                  	<span><i class="fas fa-star"></i></span>
+                                  	<span><i class="far fa-star"></i></span>
+                                </div>
+                                <div class="price py-2">
+                                  	<span>$152</span>
+                                </div>
+                                <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
+                        	</div>
+                        </div>
+					</div> -->
+					<div class="col-md-12 verProductosClick jar" style="cursor:pointer;">
+                        <div class="blog-card blog-card-blog">
+                            <div class="blog-card-image text-center">
+                                <a href="#"> <img class="img" src="<?= base_url('').$row->imageurl?>" style=" width: 200px; height:200px;"> </a>
+                                <div class="ripple-cont"></div>
+                            </div>
+                            <div class="blog-table">
+                                <h6 class="blog-category blog-text-success"><i class="far fa-newspaper"></i> </h6>
+                                <h4 class="blog-card-caption">
+                                    <a href="#" class="text-success"><?= $row->nomart?></a>
+                                </h4>
+                                <p class="blog-card-description">...</p>
+                                <div class="ftr">
+                                    <div class="author">
+                                        <p class="pull-left text-success">$</p>
+                                        
+                                    </div>
+                                    <p class="pull-right ">Und: <del></del></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+					<?php endforeach; ?>
+                </div>
+                <!-- !owl carousel -->
+            </div>
+        </section>
 
+	<!-- TODOS LOS PRODUCTOS-->
 	<br><br>
 	<div class="row" id="contenido"></div>
 	<div class="row" id="paginacion"></div>
@@ -200,3 +263,110 @@
 	</script>
 
 	<script src="<?php echo base_url('asset/clientes/ajax/principal/principal.js')?>" type="text/javascript"></script>
+ <!-- Owl Carousel Js file -->
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha256-pTxD+DSzIwmwhOqTFN+DB+nHjO4iAsbgfyFq5K5bcE0=" crossorigin="anonymous"></script>
+
+<!--  isotope plugin cdn  -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.6/isotope.pkgd.min.js" integrity="sha256-CBrpuqrMhXwcLLUd5tvQ4euBHCdh7wGlDfNz8vbu/iI=" crossorigin="anonymous"></script>
+<script>
+	$(document).ready(function(){
+
+// banner owl carousel
+$("#banner-area .owl-carousel").owlCarousel({
+	dots: true,
+	items: 1
+});
+
+// top sale owl carousel
+$("#top-sale .owl-carousel").owlCarousel({
+	loop: true,
+	nav: true,
+	dots: false,
+	responsive : {
+		0: {
+			items: 1
+		},
+		600: {
+			items: 3
+		},
+		1000 : {
+			items: 5
+		}
+	}
+});
+
+// isotope filter
+var $grid = $(".grid").isotope({
+	itemSelector : '.grid-item',
+	layoutMode : 'fitRows'
+});
+
+// filter items on button click
+$(".button-group").on("click", "button", function(){
+	var filterValue = $(this).attr('data-filter');
+	$grid.isotope({ filter: filterValue});
+})
+
+
+// new phones owl carousel
+$("#new-phones .owl-carousel").owlCarousel({
+	loop: true,
+	nav: false,
+	dots: true,
+	responsive : {
+		0: {
+			items: 1
+		},
+		600: {
+			items: 3
+		},
+		1000 : {
+			items: 5
+		}
+	}
+});
+
+// blogs owl carousel
+$("#blogs .owl-carousel").owlCarousel({
+	loop: true,
+	nav: false,
+	dots: true,
+	responsive : {
+		0: {
+			items: 1
+		},
+		600: {
+			items: 3
+		}
+	}
+})
+
+
+// product qty section
+let $qty_up = $(".qty .qty-up");
+let $qty_down = $(".qty .qty-down");
+// let $input = $(".qty .qty_input");
+
+// click on qty up button
+$qty_up.click(function(e){
+	let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+	if($input.val() >= 1 && $input.val() <= 9){
+		$input.val(function(i, oldval){
+			return ++oldval;
+		});
+	}
+});
+
+   // click on qty down button
+   $qty_down.click(function(e){
+	let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+	if($input.val() > 1 && $input.val() <= 10){
+		$input.val(function(i, oldval){
+			return --oldval;
+		});
+	}
+});
+
+
+});
+</script>
